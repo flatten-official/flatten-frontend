383a1cd6285befd667e28861f8b08574420eed98
William Wen <william.wen360@gmail.com>
2020-04-05 22:05:51 -0400

    Added esri page and button at bottom of footer

---
+++ src/components/esri_gis_map/Esri_Gis_Map.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Heatmap extends React.Component {
+  render() {
+    return <div>Hello Darkness My Old Friend</div>;
+  }
+}
+
+export default Heatmap;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,58 @@
 import React from "react";
+import PrimaryButton from "../common/buttons/PrimaryButton";
 
 class Footer extends React.Component {
   render() {
     return (
-    <div className="footer">
+      <div className="footer">
         <div className="footer__container">
-        <div className="footer__pair">
+          <div className="footer__pair">
             <div className="footer__section body">
-                <p><b>Feedback Form</b></p>
-                <a><p>TBD</p></a>
+              <p>
+                <b>Feedback Form</b>
+              </p>
+              <a>
+                <p>TBD</p>
+              </a>
             </div>
             <div className="footer__section body">
-                <p><b>Additional Information</b></p>
-                <a><p>About Us</p></a>
-                <a><p>Sponsors</p></a>
+              <p>
+                <b>Additional Information</b>
+              </p>
+              <a>
+                <p>About Us</p>
+              </a>
+              <a>
+                <p>Sponsors</p>
+              </a>
             </div>
+          </div>
+          <div className="footer__pair">
+            <div className="footer__section body">
+              <p>
+                <b>Legal</b>
+              </p>
+              <a>
+                <p>Terms of Service</p>
+              </a>
+              <a>
+                <p>Privacy Policy</p>
+              </a>
+            </div>
+            <div className="footer__section body">
+              <p>
+                <b>Contact Us</b>
+              </p>
+              <a>
+                <p>flattenofficial@gmail.com</p>
+              </a>
+            </div>
+          </div>
+          <PrimaryButton className="symptoms__submit-button">
+            View Detailed Maps and Statistics
+          </PrimaryButton>
         </div>
-        <div className="footer__pair">
-            <div className="footer__section body">
-                <p><b>Legal</b></p>
-                <a><p>Terms of Service</p></a>
-                <a><p>Privacy Policy</p></a>
-            </div>
-            <div className="footer__section body">
-                <p><b>Contact Us</b></p>
-                <a><p>flattenofficial@gmail.com</p></a>
-            </div>
-        </div>
-            
-        </div>
-    </div>
+      </div>
     );
   }
 }
