3bf368a232a8bbb1d39915e49c057afea30d47c5
Eric Zhai <e.b.zhai@gmail.com>
2020-04-07 19:28:13 -0400

    extract modal component

--- src/components/home/ReturningUserModal.jsx
+++ src/components/home/ReturningUserModal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import Modal from "../common/modal/Modal";
 import TextInput from "../common/fields/TextInput";
 import PrimaryButton from "../common/buttons/PrimaryButton";
 import { validate, isValidEmail } from "../../utils/formValidation";
-import CloseIcon from "../../assets/close.svg";
 
 export const returningUserFormName = "returningUserForm";
 
@@ -17,28 +17,25 @@ const emailValidation = [
 
 const ReturningUserModal = ({ handleSubmit, onClose }) => {
   return (
-    <div className="returning-user modal">
-      <div className="returning-user__container">
-        <CloseIcon className="returning-user__close-icon" onClick={onClose} />
-        <div className="returning-user__title title">
-          Returning user?
-        </div>
-        <div className="returning-user__body body">
-          <form onSubmit={handleSubmit}>
-            <Field
-              name="email"
-              label="Email"
-              component={TextInput}
-            />
-            <div className="returning-user__submit">
-              <PrimaryButton className="returning-user__submit-button">
-                Sign In
-              </PrimaryButton>
-            </div>
-          </form>
-        </div>
+    <Modal className="returning-user" onClose={onClose}>
+      <div className="returning-user__title title">
+        Returning user?
       </div>
-    </div>
+      <div className="returning-user__body body">
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="email"
+            label="Email"
+            component={TextInput}
+          />
+          <div className="returning-user__submit">
+            <PrimaryButton className="returning-user__submit-button">
+              Sign In
+            </PrimaryButton>
+          </div>
+        </form>
+      </div>
+    </Modal>
   );
 };
 
