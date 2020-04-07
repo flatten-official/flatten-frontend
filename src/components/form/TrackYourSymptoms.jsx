25737bfc1d081076b4ed2de74d169405275ccc80
Emily <tao.emily@yahoo.ca>
2020-04-06 23:51:18 -0400

    Fixing responsiveness on mobile for modal

--- src/components/form/TrackYourSymptoms.jsx
+++ src/components/form/TrackYourSymptoms.jsx
@@ -10,37 +10,48 @@ const TrackYourSymptoms = () => {
   const [submitting, setSubmitting] = useState(false);
   const dispatch = useDispatch();
 
-  const handleClick = async () => {
-    setSubmitting(true);
-    await dispatch(submit(symptomsFormName));
-    setSubmitting(false);
-  }
+  const handleClick = () => {
+    dispatch(submit(symptomsFormName));
+  };
 
-  const handleSubmit = values => {
-    dispatch(submitForm(values));
-  }
+  const handleSubmit = async (values) => {
+    setShowModal(true);
+    await dispatch(submitForm(values));
+  };
+
+  const handleSubmitSuccess = () => {
+    // once the submitForm action is working, close the modal if submission was successful
+    // setShowModal(false);
+  };
 
   return (
     <div className="symptoms">
       <div className="symptoms__header">
         <div className="symptoms__title">
           <div className="title">Tell us how you feel!</div>
-          <SyringeIcon className="symptoms__syringe-icon"/>
+          <SyringeIcon className="symptoms__syringe-icon" />
         </div>
         <p className="symptoms__description body">
-          Your answers will be collected and shared anonymously through our heatmap to help
-          healthcare providers, researchers, and community members gauge the spread of COVID-19.
-          The questionnaire is based on the best available guidance from Canadian public health
-          agencies, and is designed to collect information regarding your risk factors for
-          COVID-19. It is not intended to facilitate any kind of diagnosis or self-assessment for 
-          COVID-19. If you suspect you may have COVID-19, please seek a medical professional. See
-          our Terms of Service and Privacy Policy for more information.
+          Your answers will be collected and shared anonymously through our
+          heatmap to help healthcare providers, researchers, and community
+          members gauge the spread of COVID-19. The questionnaire is based on
+          the best available guidance from Canadian public health agencies, and
+          is designed to collect information regarding your risk factors for
+          COVID-19. It is not intended to facilitate any kind of diagnosis or
+          self-assessment for COVID-19. If you suspect you may have COVID-19,
+          please seek a medical professional. See our Terms of Service and
+          Privacy Policy for more information.
         </p>
         <p className="symptoms__description body">
-          <b>Please fill in this form even if you are experiencing no symptoms.</b>
+          <b>
+            Please fill in this form even if you are experiencing no symptoms.
+          </b>
         </p>
       </div>
-      <SymptomsForm onSubmit={handleSubmit}/>
+      <SymptomsForm
+        onSubmit={handleSubmit}
+        onSubmitSuccess={handleSubmitSuccess}
+      />
       <div className="symptoms__submit">
         <PrimaryButton
           className="symptoms__submit-button"
@@ -50,6 +61,7 @@ const TrackYourSymptoms = () => {
           Submit
         </PrimaryButton>
       </div>
+      {showModal && <SubmitModal onClose={() => setShowModal(false)} />}
     </div>
   );
 };
