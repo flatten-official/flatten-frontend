4df79191b685e310347cf5ce627f7cd24c6ad791
Yifei Zhang <yifei7.zhang@gmail.com>
2020-04-07 22:00:57 -0400

    fix

--- src/components/home/ReturningUserModal.jsx
+++ src/components/home/ReturningUserModal.jsx
@@ -1,32 +1,51 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import Recaptcha from "react-recaptcha";
+
 import Modal from "../common/modal/Modal";
 import TextInput from "../common/fields/TextInput";
 import PrimaryButton from "../common/buttons/PrimaryButton";
 import { validate, isValidEmail } from "../../utils/formValidation";
+import { RecaptchaKey } from "../form/Recaptcha.js";
 
 export const returningUserFormName = "returningUserForm";
 
 const emailValidation = [
-  [
-    "email",
-    "Please enter a valid email",
-    ({ email }) => isValidEmail(email)
-  ]
+  ["email", "Please enter a valid email", ({ email }) => isValidEmail(email)],
+  ["recaptchaVerification", "Please prove you are not a robot."],
 ];
 
 const ReturningUserModal = ({ handleSubmit, onClose }) => {
+  const recaptchaLoaded = () => {
+    //console.log("Loaded");
+  };
+
+  const handleRecaptchaExpired = () => {
+    change("recaptchaVerification", false);
+  };
+
+  const handleRecaptchaVerified = (response) => {
+    if (response) {
+      change("recaptchaVerification", response);
+    }
+  };
   return (
     <Modal className="returning-user" onClose={onClose}>
-      <div className="returning-user__title title">
-        Returning user?
-      </div>
+      <div className="returning-user__title title">Returning user?</div>
       <div className="returning-user__body body">
         <form onSubmit={handleSubmit}>
+          <Field name="email" label="Email" component={TextInput} />
+          <Recaptcha
+            sitekey={RecaptchaKey()}
+            render="explicit"
+            onloadCallback={recaptchaLoaded}
+            verifyCallback={handleRecaptchaVerified}
+            expiredCallback={handleRecaptchaExpired}
+          />
           <Field
-            name="email"
-            label="Email"
             component={TextInput}
+            name="recaptchaVerification"
+            type="hidden"
           />
           <div className="returning-user__submit">
             <PrimaryButton className="returning-user__submit-button">
@@ -41,5 +60,5 @@ const ReturningUserModal = ({ handleSubmit, onClose }) => {
 
 export default reduxForm({
   form: returningUserFormName,
-  validate: validate(emailValidation)
+  validate: validate(emailValidation),
 })(ReturningUserModal);
