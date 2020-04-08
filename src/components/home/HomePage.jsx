5267f05cd179a13cfafd9f833bf13d7a0de1b3ae
Yifei Zhang <yifei7.zhang@gmail.com>
2020-04-07 23:06:29 -0400

    homepage fix

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -7,7 +7,6 @@ import ReturningUserModal from "./ReturningUserModal";
 import PrimaryButton from "../common/buttons/PrimaryButton";
 import logo from "../../assets/logo-black.png";
 
-
 import { animateScroll as scroll, scroller } from "react-scroll";
 
 const scrollToForm = () => {
@@ -15,7 +14,7 @@ const scrollToForm = () => {
     duration: 800,
     delay: 0,
     smooth: "easeInOutQuad",
-    offset: -70
+    offset: -70,
   });
 };
 
@@ -33,18 +32,10 @@ const HomePage = ({ cookieStatus, t }) => {
       <div className="home__content">
         <img className="home__logo" src={logo} alt="logo" />
         <div className="body home__description">
-          <Trans t={t} i18nKey="chunk1">
-            Flatten is designed to collect <b>real-time healthcare data</b>
-            and
-            <b> increase national awareness</b> throughout this difficult time
-            during COVID-19.
-          </Trans>
+          {t("chunk1")}
           <br />
           <br />
-          <Trans t={t} i18nKey="chunk2">
-            Our ultimate goal is to flatten the curve of COVID-19, and you can
-            do your part by <b>filling out our form once a day</b>.
-          </Trans>
+          {t("chunk2")}
         </div>
         <PrimaryButton
           className="home__button body"
