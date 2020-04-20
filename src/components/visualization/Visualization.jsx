811b1638e27ab7b9da5d671f8b2e6e1a0cb56cea
Arthur Allshire <arthur@allshire.org>
2020-04-20 16:40:04 -0400

    Starting work on visualisation

--- src/components/visualization/Visualization.jsx
+++ src/components/visualization/Visualization.jsx
@@ -1,112 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useEffect } from "react";
 import { withTranslation } from "react-i18next";
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis } from "recharts";
 import * as d3 from "d3";
+import utils from "./vis_utils.js";
 
 let height = 180;
 let margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
-// Recharts example
-let Chart1 = function() {
-  let [data, setData] = useState([]);
-  // let container = document.getElementById("chart1");
-  // let width = container ? container.clientWidth : 0;
-
-  useEffect(async () => {
-    let url =
-      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo";
-    let raw = await d3.json(url);
-    let priceData = raw["Time Series (Daily)"];
-    let dates = Object.keys(priceData);
-    let data = dates.map(date => ({
-      date: new Date(date),
-      upper: priceData[date]["2. high"]
-    }));
-    setData(data);
-  }, []);
-
-  return (
-    <ResponsiveContainer debounce={15}>
-      <LineChart data={data}>
-        <Line type="monotone" dataKey="upper" stroke="steelblue" />
-        <XAxis dataKey="date" />
-        <YAxis dataKey="upper" />
-      </LineChart>
-    </ResponsiveContainer>
-  );
-};
-
 // d3 example
-let drawChart2 = async t => {
-  let width = document.getElementById("chart2").clientWidth;
+let drawCasesChart = (t, container_selector) => {
+  let width = document.getElementById("cases-chart").offsetWidth;
 
   let svg = d3
-    .select("#chart2")
+    .select(container_selector)
     .append("svg")
     .attr("viewBox", [0, 0, width, height])
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .call(utils.responsivefy);
 
   let url =
-    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo";
-  let raw = await d3.json(url);
-  let priceData = raw["Time Series (Daily)"];
-  let dates = Object.keys(priceData);
-  let data = dates.map(date => ({
-    date: new Date(date),
-    upper: priceData[date]["2. high"]
-  }));
+    "https://storage.googleapis.com/flatten-staging-271921.appspot.com/confirmed_time_series.json";
 
-  let x = d3
-    .scaleTime()
-    .domain(d3.extent(data, d => d.date))
-    .range([margin.left, width - margin.right]);
+  d3.json(url).then((raw) => {
+    // let raw = await d3.json(url);
+    let priceData = raw["ONTARIO"]["Time Series (Daily)"];
+    let dates = Object.keys(priceData);
+    let data = dates.map((date) => ({
+      date: new Date(date),
+      upper: priceData[date]["Total Deaths"],
+    }));
 
-  let y = d3
-    .scaleLinear()
-    .domain(d3.extent(data, d => d.upper))
-    .range([height - margin.bottom, margin.top]);
+    let x = d3
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([margin.left, width - margin.right]);
 
-  let line = d3
-    .line()
-    .x(d => x(d.date))
-    .y(d => y(d.upper));
+    let y = d3
+      .scaleLinear()
+      .domain(d3.extent(data, (d) => d.upper))
+      .range([height - margin.bottom, margin.top]);
 
-  let xAxis = g =>
-    g.attr("transform", `translate(0,${height - margin.bottom})`).call(
-      d3
-        .axisBottom(x)
-        .ticks(width / 80)
-        .tickSizeOuter(0)
-    );
+    let line = d3
+      .line()
+      .x((d) => x(d.date))
+      .y((d) => y(d.upper));
 
-  let yAxis = g =>
-    g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y).ticks(height / 40))
-      .call(g => g.select(".domain").remove());
+    let xAxis = (g) =>
+      g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+        d3
+          .axisBottom(x)
+          .ticks(width / 80)
+          .tickSizeOuter(0)
+      );
 
-  svg.append("g").call(xAxis);
+    let yAxis = (g) =>
+      g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y).ticks(height / 40))
+        .call((g) => g.select(".domain").remove());
 
-  svg.append("g").call(yAxis);
+    svg.append("g").call(xAxis);
 
-  svg
-    .append("path")
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("d", line(data));
+    svg.append("g").call(yAxis);
+
+    svg
+      .append("path")
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("d", line(data));
+  });
 };
 
 function Visualization({ t }) {
   useEffect(() => {
-    drawChart2(t);
+    drawCasesChart(t, "#cases-chart");
+    drawCasesChart(t, "#cases-chart2");
   }, []);
   return (
     <div className="visualization">
-      <div id="chart1" className="chart">
-        <Chart1 />
-      </div>
-      <div id="chart2" className="chart" />
+      <div id="cases-chart" className="chart" />
+      <div id="cases-chart2" className="chart" />
     </div>
   );
 }
