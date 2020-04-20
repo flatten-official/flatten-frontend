24684665596e314095e254b372bfa660df163786
Yifei Zhang <yifei7.zhang@gmail.com>
2020-04-19 21:39:18 -0400

    bring back post-submit modal

---
+++ src/components/form/SubmitModal.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { scroller } from "react-scroll";
+import Modal from "../common/modal/Modal";
+import GiftIcon from "../../assets/gift.svg";
+import FacebookButton from "../common/buttons/FacebookButton";
+import TwitterButton from "../common/buttons/TwitterButton";
+
+const SubmitModal = ({ onClose }) => {
+  useEffect(
+    () => () => {
+      scroller.scrollTo("heatmap", {
+        duration: 1000,
+        smooth: true,
+        offset: -70,
+      });
+    },
+    []
+  );
+
+  return (
+    <Modal className="submit-modal" onClose={onClose}>
+      <div className="submit-modal__content body">
+        <b>Thank you for submitting our form!</b>
+        <br />
+        Exit to go to the heatmap.
+      </div>
+      <GiftIcon className="submit-modal__gift-icon" />
+      <div className="submit-modal__share">
+        <FacebookButton />
+        <TwitterButton />
+      </div>
+    </Modal>
+  );
+};
+
+export default SubmitModal;
