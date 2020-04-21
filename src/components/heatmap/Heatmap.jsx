3e4c8bb3422a1d0d042e57d88881666b77747fd0
Miraal Kabir <miraalkabir@Miraals-MacBook-Pro.local>
2020-04-20 22:50:21 -0400

    new heatmap section

--- src/components/heatmap/Heatmap.jsx
+++ src/components/heatmap/Heatmap.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Trans, withTranslation } from "react-i18next";
 
-import MapIcon from "../../assets/map.svg";
 import Leafletmap from "./Leafletmap";
 
 class HeatMap extends React.Component {
@@ -38,51 +37,44 @@ class HeatMap extends React.Component {
     return (
       <div className="heatmap" id="heatmap">
         <div className="heatmap__header">
-          <div className="heatmap__title">
-            <div className="title">{t("header")}</div>
-            <MapIcon className="heatmap__map-icon"></MapIcon>
-          </div>
-
-          <div className="heatmap__description body">
-            <p>{t("p1")}</p>
-          </div>
-
-          <div className="heatmap__description body">
-            <p>
-              <Trans t={t} i18nKey="p2">
-                <b>High-Risk Potential Cases:</b> This tab displays all
-                individuals that are especially vulnerable to COVID-19 in Canada
-                that are also deemed potential cases, based on data inputted
-                into Flatten's form.
-              </Trans>
-            </p>
-
-            <p>
-              <Trans t={t} i18nKey="p3">
-                <b>Potential Cases:</b> This tab displays all of the potential
-                cases of COVID-19 in Canada, based on data inputted into
-                Flatten's form.
-              </Trans>
-            </p>
-
-            <p>
-              <Trans t={t} i18nKey="p4">
-                <b>Vulnerable Individuals:</b> This tab displays all individuals
-                that are especially vulnerable to COVID-19 in Canada, based on
-                data inputted into Flatten's form.
-              </Trans>
-            </p>
-          </div>
-
-          <div className="heatmap__description body">
-            <p>
-              <i>{t("p5")}</i>
-            </p>
+          &nbsp; &nbsp;
+          <div className="heatmap__headercontainer">
+            <div className="heatmap__title">
+              <div className="title">{t("header")}</div>
+            </div>
+            <div className="heatmap__description body">
+              <p>
+                <b>{t("p1")}</b>
+                <br></br>
+                <br></br>
+                <div className="heatmap__minidescription body">{t("p9")}</div>
+              </p>
+            </div>
           </div>
         </div>
 
         <div className="heatmap__container">
           <Leafletmap></Leafletmap>
+        </div>
+
+        <div className="heatmap__header">
+          <div className="heatmap__headercontainer">
+            <div className="heatmap__description body">
+              <p>
+                <b>{t("p6")}</b>
+                <br></br>
+                <b>{t("p7")}</b>
+              </p>
+            </div>
+            <div className="heatmap__minidescription body">
+              <p>
+                {t("p8")}
+                <br></br>
+                <br></br>
+                {t("p5")}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
