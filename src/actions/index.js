41f97fa3baa0ea0e0678d83d10bc6bfff1ca6fc6
Martin Staadecker <machstg@gmail.com>
2020-04-23 14:37:03 -0400

    Update actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,45 +2,36 @@ import backend from "../apis/backend";
 import i18next from "i18next";
 import history from "../history";
 
+export const COOKIE_EXISTS = "COOKIE_EXISTS";
+export const CURRENT_LANG = "CURRENT_LANG";
+export const LANG_CHANGE = "LANG_CHANGE";
+export const SET_COOKIE = "SET_COOKIE";
+
 export const readCookie = () => async (dispatch) => {
   const { data } = await backend.get("/read-cookie");
-  dispatch({
-    type: "COOKIE_EXISTS",
-    payload: data,
-  });
+  dispatch({ type: COOKIE_EXISTS, payload: data });
 };
 
 export const getGeolocation = () => async (dispatch) => {
-  let lang = i18next.language;
   try {
     const response = await backend.get("/locale");
     const params = new URLSearchParams(window.location.search);
+
     if (!params.get("lang")) {
-      lang = response.data.locale;
+      const lang = response.data.locale;
       history.push(`${window.location.pathname}?lang=${lang}`);
       await i18next.changeLanguage(lang).catch(console.error);
-      dispatch({
-        type: "LANG_CHANGE",
-        payload: lang,
-      });
+      dispatch({ type: LANG_CHANGE, payload: lang });
     } else {
-      dispatch({
-        type: "CURRENT_LANG",
-        payload: lang,
-      });
+      dispatch({ type: CURRENT_LANG, payload: i18next.language });
     }
   } catch (e) {
     console.log(e);
-    dispatch({
-      type: "CURRENT_LANG",
-      payload: false,
-    });
+    dispatch({ type: CURRENT_LANG, payload: false });
   }
 };
 
 export const setDailyCookie = () => async (dispatch) => {
   await backend.post("/set-daily-cookie");
-  return {
-    type: "SET_COOKIE",
-  };
+  return { type: SET_COOKIE };
 };
