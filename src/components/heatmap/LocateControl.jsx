203661dbe1bcdacfdb2248d533cf2c7eb5472420
Martin Staadecker <machstg@gmail.com>
2020-04-26 03:17:30 -0400

    Simplify LocateControl.jsx

--- src/components/heatmap/LocateControl.jsx
+++ src/components/heatmap/LocateControl.jsx
@@ -1,27 +1,17 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { withLeaflet } from "react-leaflet";
 import Locate from "leaflet.locatecontrol";
 
-const options = {
-  position: "topleft",
-  initialZoomLevel: 12,
-  onActivate: () => {}, // callback before engine starts retrieving locations
+const LocateControl = ({ leaflet }) => {
+  useEffect(() => {
+    new Locate({
+      position: "topleft",
+      initialZoomLevel: 12,
+      showPopup: false,
+    }).addTo(leaflet.map);
+  }, []);
+
+  return null;
 };
-
-class LocateControl extends Component {
-  componentDidMount() {
-    const { startDirectly, leaflet } = this.props;
-
-    const location = new Locate(options);
-    location.addTo(leaflet.map);
-
-    if (startDirectly) {
-      // request location update and set location
-      location.start();
-    }
-  }
-
-  render = () => null;
-}
 
 export default withLeaflet(LocateControl);
