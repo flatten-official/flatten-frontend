34211b9826a637aeeffbfe7c586709752db9408d
Martin Staadecker <machstg@gmail.com>
2020-04-27 17:28:26 -0400

    Extract data infos into object for readability

--- src/components/heatmap/mapConstants.js
+++ src/components/heatmap/mapConstants.js
@@ -94,6 +94,121 @@ export const TABS = {
     },
   },
 };
+const DATA_INFO = {
+  somaliaForm: {
+    url:
+      "https://storage.googleapis.com/flatten-staging-271921.appspot.com/somalia_data.json",
+    shapeType: SHAPE_TYPE.CIRCLES,
+    type: DATA_TYPE.OVERLAY,
+    baseLayer: {
+      geoJson: somaliaGeoJsonBase,
+      fields: {
+        getRegionName: (prop) => prop.NAME,
+      },
+    },
+    fields: {
+      getRegions: (obj) => obj.region,
+      getPotential: (region) => region.potential,
+      getVulnerable: (region) => region.risk,
+      getBoth: (region) => null, // TODO implement
+      getRegionalTotal: (region) => region.number_reports,
+      getTotal: (obj) => null, // TODO implement,
+    },
+    ui: {
+      circleSizes: [10, 12, 15, 17, 20, 25],
+      thresholds: [3, 12, 25, 125, 250],
+    },
+    notEnoughDataThreshold: 1,
+  },
+  somaliaConfirmed: {
+    url:
+      "https://storage.googleapis.com/flatten-staging-271921.appspot.com/somalia_confirmed.json",
+    shapeType: SHAPE_TYPE.CIRCLES,
+    type: DATA_TYPE.STANDALONE,
+    fields: {
+      getConfirmed: (prop) => prop.CONFIRMED,
+      getEnglishName: (prop) => prop.COUNTRY,
+    },
+    ui: {
+      circleSizes: [10, 12, 15, 17, 20, 25],
+      thresholds: [3, 12, 25, 125, 250],
+    },
+  },
+  canadaConfirmed: {
+    type: DATA_TYPE.STANDALONE,
+    shapeType: SHAPE_TYPE.POLYGONS,
+    url:
+      "https://opendata.arcgis.com/datasets/e5403793c5654affac0942432783365a_0.geojson",
+    fields: {
+      getEnglishName: (prop) => prop.ENGNAME,
+      getFrenchName: (prop) => prop.FRENAME,
+      getConfirmed: (prop) => prop.CaseCount,
+      getLastUpdated: (prop) => prop.Last_Updated,
+    },
+  },
+  canadaForm: {
+    url:
+      "https://storage.googleapis.com/flatten-271620.appspot.com/form_data.json",
+    type: DATA_TYPE.OVERLAY,
+    shapeType: SHAPE_TYPE.POLYGONS,
+    baseLayer: {
+      geoJson: canadaGeoJsonBase,
+      fields: {
+        getRegionName: (prop) => prop.CFSAUID,
+      },
+    },
+    fields: {
+      getRegions: (obj) => obj.fsa,
+      getTotal: (obj) => obj.total_responses,
+      getTimestamp: (obj) => obj.time,
+      getPotential: (region) => region.pot,
+      getVulnerable: (region) => region.risk,
+      getBoth: (region) => region.both,
+      getRegionalTotal: (region) => region.number_reports,
+    },
+    notEnoughDataThreshold: 25,
+  },
+  usaConfirmed: {
+    type: DATA_TYPE.STANDALONE,
+    shapeType: SHAPE_TYPE.CIRCLES,
+    url:
+      "https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson",
+    fields: {
+      getEnglishName: (prop) => prop.Combined_Key,
+      getConfirmed: (prop) => prop.Confirmed,
+      getLastUpdated: (prop) => prop.Last_Update,
+    },
+    ui: {
+      circleSizes: [3, 5, 10, 15, 20, 25],
+      thresholds: [100, 500, 1000, 5000, 10000],
+    },
+  },
+  usaForm: {
+    url:
+      "https://storage.googleapis.com/flatten-271620.appspot.com/form_data_usa.json",
+    type: DATA_TYPE.OVERLAY,
+    shapeType: SHAPE_TYPE.POLYGONS,
+    baseLayer: {
+      geoJson: usaGeoJsonBase,
+      fields: {
+        getRegionName: (prop) => prop.NAME,
+      },
+    },
+    ui: {
+      nameSuffix: " County",
+    },
+    fields: {
+      getTimestamp: (obj) => obj.time,
+      getTotal: (obj) => obj.total_responses,
+      getRegions: (obj) => obj.county,
+      getPotential: (region) => region.pot,
+      getVulnerable: (region) => region.risk,
+      getBoth: (region) => region.both,
+      getRegionalTotal: (region) => region.number_reports,
+    },
+    notEnoughDataThreshold: 25,
+  },
+};
 
 const COUNTRIES = {
   canada: {
@@ -111,40 +226,8 @@ const COUNTRIES = {
       showDataFooter: true,
     },
     data: {
-      confirmed: {
-        type: DATA_TYPE.STANDALONE,
-        shapeType: SHAPE_TYPE.POLYGONS,
-        url:
-          "https://opendata.arcgis.com/datasets/e5403793c5654affac0942432783365a_0.geojson",
-        fields: {
-          getEnglishName: (prop) => prop.ENGNAME,
-          getFrenchName: (prop) => prop.FRENAME,
-          getConfirmed: (prop) => prop.CaseCount,
-          getLastUpdated: (prop) => prop.Last_Updated,
-        },
-      },
-      form: {
-        url:
-          "https://storage.googleapis.com/flatten-271620.appspot.com/form_data.json",
-        type: DATA_TYPE.OVERLAY,
-        shapeType: SHAPE_TYPE.POLYGONS,
-        baseLayer: {
-          geoJson: canadaGeoJsonBase,
-          fields: {
-            getRegionName: (prop) => prop.CFSAUID,
-          },
-        },
-        fields: {
-          getRegions: (obj) => obj.fsa,
-          getTotal: (obj) => obj.total_responses,
-          getTimestamp: (obj) => obj.time,
-          getPotential: (region) => region.pot,
-          getVulnerable: (region) => region.risk,
-          getBoth: (region) => region.both,
-          getRegionalTotal: (region) => region.number_reports,
-        },
-        notEnoughDataThreshold: 25,
-      },
+      confirmed: DATA_INFO.canadaConfirmed,
+      form: DATA_INFO.canadaForm,
     },
   },
   usa: {
@@ -162,46 +245,8 @@ const COUNTRIES = {
       showDataFooter: true,
     },
     data: {
-      confirmed: {
-        type: DATA_TYPE.STANDALONE,
-        shapeType: SHAPE_TYPE.CIRCLES,
-        url:
-          "https://opendata.arcgis.com/datasets/628578697fb24d8ea4c32fa0c5ae1843_0.geojson",
-        fields: {
-          getEnglishName: (prop) => prop.Combined_Key,
-          getConfirmed: (prop) => prop.Confirmed,
-          getLastUpdated: (prop) => prop.Last_Update,
-        },
-        ui: {
-          circleSizes: [3, 5, 10, 15, 20, 25],
-          thresholds: [100, 500, 1000, 5000, 10000],
-        },
-      },
-      form: {
-        url:
-          "https://storage.googleapis.com/flatten-271620.appspot.com/form_data_usa.json",
-        type: DATA_TYPE.OVERLAY,
-        shapeType: SHAPE_TYPE.POLYGONS,
-        baseLayer: {
-          geoJson: usaGeoJsonBase,
-          fields: {
-            getRegionName: (prop) => prop.NAME,
-          },
-        },
-        ui: {
-          nameSuffix: " County",
-        },
-        fields: {
-          getTimestamp: (obj) => obj.time,
-          getTotal: (obj) => obj.total_responses,
-          getRegions: (obj) => obj.county,
-          getPotential: (region) => region.pot,
-          getVulnerable: (region) => region.risk,
-          getBoth: (region) => region.both,
-          getRegionalTotal: (region) => region.number_reports,
-        },
-        notEnoughDataThreshold: 25,
-      },
+      confirmed: DATA_INFO.usaConfirmed,
+      form: DATA_INFO.usaForm,
     },
   },
   somalia: {
@@ -219,45 +264,8 @@ const COUNTRIES = {
       showDataFooter: false,
     },
     data: {
-      confirmed: {
-        url:
-          "https://storage.googleapis.com/flatten-staging-271921.appspot.com/somalia_confirmed.json",
-        shapeType: SHAPE_TYPE.CIRCLES,
-        type: DATA_TYPE.STANDALONE,
-        fields: {
-          getConfirmed: (prop) => prop.CONFIRMED,
-          getEnglishName: (prop) => prop.COUNTRY,
-        },
-        ui: {
-          circleSizes: [10, 12, 15, 17, 20, 25],
-          thresholds: [3, 12, 25, 125, 250],
-        },
-      },
-      form: {
-        url:
-          "https://storage.googleapis.com/flatten-staging-271921.appspot.com/somalia_data.json",
-        shapeType: SHAPE_TYPE.CIRCLES,
-        type: DATA_TYPE.OVERLAY,
-        baseLayer: {
-          geoJson: somaliaGeoJsonBase,
-          fields: {
-            getRegionName: (prop) => prop.NAME,
-          },
-        },
-        fields: {
-          getRegions: (obj) => obj.region,
-          getPotential: (region) => region.potential,
-          getVulnerable: (region) => region.risk,
-          getBoth: (region) => null, // TODO implement
-          getRegionalTotal: (region) => region.number_reports,
-          getTotal: (obj) => null, // TODO implement,
-        },
-        ui: {
-          circleSizes: [10, 12, 15, 17, 20, 25],
-          thresholds: [3, 12, 25, 125, 250],
-        },
-        notEnoughDataThreshold: 1,
-      },
+      confirmed: DATA_INFO.somaliaConfirmed,
+      form: DATA_INFO.somaliaForm,
     },
   },
 };
