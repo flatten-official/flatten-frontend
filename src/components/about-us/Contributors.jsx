7e44a37defed70c1d35148351729650aa3a40a50
raschamp7 <33166653+raschamp7@users.noreply.github.com>
2020-06-12 12:19:38 -0400

    Revert "Revert "Simplify Contributors.jsx""
    This reverts commit 0bd9f3bf284ab03cf0e7cfd484f72d93896ce9f2.

--- src/components/about-us/Contributors.jsx
+++ src/components/about-us/Contributors.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-// In future should add webp img's to serve in order to improve img loading times
-// Need to add a webp loader to the webpack config
 const Contributors = ({ name }) => (
   <>
-    <div className="OldProfile__card">
-      <p className="OldProfile__name">{name}</p>
-    </div>
-    <br />
+    <p className="OldProfile__name">{name}</p>
   </>
 );
+
+Contributors.propType = {
+  name: PropTypes.string,
+};
 
 export default Contributors;
