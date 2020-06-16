ba56922617b5f9ec8249fcac6c81d3b71f6d3c9e
Martin Staadecker <machstg@gmail.com>
2020-06-16 12:02:03 -0400

    Remove uneccesary fragment

--- src/components/about-us/AboutUs.jsx
+++ src/components/about-us/AboutUs.jsx
@@ -14,35 +14,33 @@ const AboutUs = ({ t }) => {
   }, []);
 
   return (
-    <React.Fragment>
-      <div className="about-us__container">
-        <section className="about-us__header">
-          <h4 className="about-us__title title">{t("header")}</h4>
-          <div className="about-us__description">
-            <p className="body">{t("p1")}</p>
-            <p className="body">
-              {t("contact")}
-              <a className="email-link" href="mailto:flattenofficial@gmail.com">
-                flattenofficial@gmail.com
-              </a>
-            </p>
-          </div>
-        </section>
-
-        <div className="about-us__body">
-          {people.team.map((person, index) => (
-            <Profile key={index} t={t} person={person} />
-          ))}
+    <div className="about-us__container">
+      <section className="about-us__header">
+        <h4 className="about-us__title title">{t("header")}</h4>
+        <div className="about-us__description">
+          <p className="body">{t("p1")}</p>
+          <p className="body">
+            {t("contact")}
+            <a className="email-link" href="mailto:flattenofficial@gmail.com">
+              flattenofficial@gmail.com
+            </a>
+          </p>
         </div>
+      </section>
 
-        <h4 className="about-us__title title">{t("contributorsHeader")}</h4>
-        <div className="about-us__body">
-          {people.contributors.map((name, index) => (
-            <Contributors key={index} name={name} />
-          ))}
-        </div>
+      <div className="about-us__body">
+        {people.team.map((person, index) => (
+          <Profile key={index} t={t} person={person} />
+        ))}
       </div>
-    </React.Fragment>
+
+      <h4 className="about-us__title title">{t("contributorsHeader")}</h4>
+      <div className="about-us__body">
+        {people.contributors.map((name, index) => (
+          <Contributors key={index} name={name} />
+        ))}
+      </div>
+    </div>
   );
 };
 
