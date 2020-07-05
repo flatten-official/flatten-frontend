74969a7941c71775df0812ad587600a390a17e73
raschamp7 <33166653+raschamp7@users.noreply.github.com>
2020-07-05 19:31:57 -0400

    reformatted to make text show better

--- src/components/media/Outlet.jsx
+++ src/components/media/Outlet.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import { ReactTinyLink } from "react-tiny-link";
 
 const Profile = ({ t, outlet }) => (
-  <div className="profile__card">
-    <div className="description">
-      <p className="profile__name">{outlet.name}</p>
-      <p className="profile__role">{outlet.date}</p>
-    </div>
+  <div className="outlet__card">  
     <ReactTinyLink
-      cardSize="large"
+      cardSize="small"
       showGraphic={true}
       maxLine={0}
       minLine={0}
